4db62f268f8dc79df232b2c50fd3341d21b99552
João Alencastro <joao.alencastro@gmail.com>
2019-03-16 12:15:53 -0300

    Parser funciona com logs snort alert_full

---
+++ Pratica/snortAlertScript/parser.py
@@ -0,0 +1,38 @@
+# -*- coding: UTF-8 -*-
+
+import pyparsing as pyp
+import itertools
+from os import listdir
+
+integer = pyp.Word(pyp.nums)
+ip_addr = pyp.Combine(integer+'.'+integer+'.'+integer+'.'+integer)
+
+def snort_parse(logfile):
+    header = (pyp.Suppress("[**] [")
+              + pyp.Combine(integer + ":" + integer + ":" + integer)
+              + pyp.Suppress(pyp.SkipTo("[**]", include = True)))
+    cls = (
+        pyp.Suppress(pyp.Optional(pyp.Literal("[Classification:")))
+        + pyp.Regex("[^]]*") + pyp.Suppress(']'))
+
+    pri = pyp.Suppress("[Priority:") + integer + pyp.Suppress("]")
+    date = pyp.Combine(
+        integer+"/"+integer+'-'+integer+':'+integer+':'+integer+'.'+integer)
+    src_ip = ip_addr + pyp.Suppress("->")
+    dest_ip = ip_addr
+
+    bnf = header+cls+pri+date+src_ip+dest_ip
+
+    with open(logfile) as snort_logfile:
+        for has_content, grp in itertools.groupby(
+                snort_logfile, key = lambda x: bool(x.strip())):
+            if has_content:
+                tmpStr = ''.join(grp)
+                fields = bnf.searchString(tmpStr)
+                if len(fields) != 0:
+                    print(fields)
+
+
+
+#print(listdir(r'C:\Users\João\Documents\GitHub\IDS\Pratica\snortAlertScript'))
+snort_parse('alert.full')
